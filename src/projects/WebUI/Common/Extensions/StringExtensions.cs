06e4016a1e49c897067ae742192d3cb595332b09
Jin Li <jinlmsft@hotmail.com>
2019-03-13 18:48:48 -0700

    Update

--- src/projects/WebUI/Common/Extensions/StringExtensions.cs
+++ src/projects/WebUI/Common/Extensions/StringExtensions.cs
@@ -9,20 +9,30 @@ namespace Common.Extensions
         public static string FromJSBase64(this string content)
         {
             // convert spaces to pluses and trim base64 spacers
-            char[] charDoc = content.Replace(' ', '+').TrimEnd(new char[] { '=' }).ToCharArray();
+            char[] charDoc = content.TrimEnd(new char[] { '=' }).ToCharArray();
 
             StringBuilder docBuilder = new StringBuilder();
             for (int index = 0; index < charDoc.Length; index++)
             {
-                if ((index % 78 == 76) && (index < charDoc.Length - 1) && charDoc[index] == '+' && charDoc[index + 1] == '+')
+                if ((index % 78 == 76) && (index < charDoc.Length - 1) && charDoc[index] == ' ' && charDoc[index + 1] == ' ')
                 {
-                    index++;
+                    index++; // Skip two 
                     continue;
                 }
-                docBuilder.Append(charDoc[index]);
+                if ( Char.IsLetterOrDigit(charDoc[index]) || charDoc[index]=='+' || charDoc[index] == '/' )
+                { 
+                    docBuilder.Append(charDoc[index]);
+                } else if (charDoc[index]==' ')
+                {
+                    docBuilder.Append('+');
+                } else
+                {
+                    Console.Write(charDoc[index]);
+                }
             }
             // Add padding, if needed--replicates 0-2 equals
             docBuilder.Append(new string('=', (4 - docBuilder.Length % 4) % 4));
+            
             return docBuilder.ToString();
         }
 
