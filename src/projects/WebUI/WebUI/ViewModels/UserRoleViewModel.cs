b2543cc78fcecf3e9b4d213cef548b43af449496
kelvin <869071540@qq.com>
2019-11-22 14:52:25 +0800

    add role controllers

---
+++ src/projects/WebUI/WebUI/ViewModels/UserRoleViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Heavy.Web.ViewModels
+{
+    public class UserRoleViewModel
+    {
+        public UserRoleViewModel()
+        {
+            Users = new List<string>();
+        }
+
+        public string UserName { get; set; }
+        public string RoleId { get; set; }
+
+        public List<string> Users { get; set; }
+    }
+}
