f26fd3cd1b8f1c0a109939385dc4063b23d20668
dragon0486 <869071540@qq.com>
2019-11-24 23:01:33 +0800

    add auth tag helper

---
+++ src/projects/WebUI/WebUI/TagHelpers/AuthTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebUI.TagHelpers
+{
+    [HtmlTargetElement(Attributes = nameof(IsAdmin))]
+    public class AuthTagHelper:TagHelper
+    {
+        public bool IsAdmin { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!IsAdmin)
+            {
+                output.SuppressOutput();
+            }
+        }
+    }
+}
