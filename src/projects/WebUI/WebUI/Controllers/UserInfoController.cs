b0ee613b4cb51d4c0529315698b1a1464c728305
kelvin <869071540@qq.com>
2019-12-19 12:05:59 +0800

    add user info api

---
+++ src/projects/WebUI/WebUI/Controllers/UserInfoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.Utils;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using WebUI.Services;
+
+namespace WebUI.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [EnableCors("dev-use")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserInfoController:ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var userId = HttpContext.User.Identity.Name;
+            var obj = await AzureService.FindUserInfo(userId);
+            return Content(new Response { Successful = "true", Msg = "", Data = obj }.JObjectToString());
+        }
+    }
+}
