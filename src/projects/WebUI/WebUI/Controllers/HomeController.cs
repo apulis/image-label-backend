e05368258e7f63ded70559ee26674b3dbd635654
kelvin <869071540@qq.com>
2020-01-02 09:56:01 +0800

    update label model

--- src/projects/WebUI/WebUI/Controllers/HomeController.cs
+++ src/projects/WebUI/WebUI/Controllers/HomeController.cs
@@ -52,6 +52,7 @@ namespace WebUI.Controllers
             HttpContext.Session.Remove(Constants.JsontagRole);
 
         }
+        [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
             await GetRole();
