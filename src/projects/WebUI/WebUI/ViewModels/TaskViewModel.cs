e4daa8348f2f51279f41377ced9f5d668bfd5bc9
dragon0486 <869071540@qq.com>
2020-01-05 12:28:22 +0800

    add response docs

---
+++ src/projects/WebUI/WebUI/ViewModels/TaskViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.ViewModels
+{
+    public class TaskViewModel
+    {
+        public string id { get; set; }
+        public int createTime { get; set; }
+        public int updateTime { get; set; }
+    }
+}
