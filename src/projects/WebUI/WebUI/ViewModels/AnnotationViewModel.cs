0033a0a4668c934972ffe9f76f542988c743c6f2
dragon0486 <869071540@qq.com>
2020-03-04 17:30:34 +0800

    update service api

---
+++ src/projects/WebUI/WebUI/ViewModels/AnnotationViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.ViewModels
+{
+    public class AnnotationViewModel
+    {
+        public List<ImageMetaInfo> images { get; }
+        public List<AnnatationInfo> annotations { get; }
+    }
+
+    public class ImageMetaInfo
+    {
+        public int license { get; set; }
+        public string file_name { get; set; }
+        public string coco_url { get; set; }
+        public int height { get; set; }    
+        public int width { get; set; }    
+        public string date_captured { get; set; }    
+        public string flickr_url { get; set; }    
+        public int id { get; set; }    
+    }
+
+    public class AnnatationInfo
+    {
+        public List<int> segmentation { get; }
+        public int area { get; }
+        public int iscrowd { get; }
+        public int image_id { get; }
+        public List<int> bbox { get; }
+        public int category_id { get; }
+        public int id { get; }
+
+    }
+}
