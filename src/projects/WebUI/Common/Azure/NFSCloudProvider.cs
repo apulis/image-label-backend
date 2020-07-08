72ebfa9c8fe1fdfa21ecce21988db4f07097ba7c
dragon0486 <869071540@qq.com>
2020-07-08 17:23:01 +0800

    update

--- src/projects/WebUI/Common/Azure/NFSCloudProvider.cs
+++ src/projects/WebUI/Common/Azure/NFSCloudProvider.cs
@@ -104,6 +104,7 @@ namespace WebUI.Azure
         public override void LinkPath(string dataPath)
         {
             string path = Path.Combine(_basePath, _directoryPath);
+            new DirectoryInfo(Path.GetDirectoryName(path)).Create();
             ProcessStartInfo startInfo = new ProcessStartInfo() { FileName = "/bin/bash", Arguments = $"-c \"ln -s {dataPath} {path}\"", };
             Process proc = new Process() { StartInfo = startInfo, };
             proc.Start();
